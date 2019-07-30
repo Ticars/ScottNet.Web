05bd066c1af094d501009900cc8a37f447da18e3
Scott Ticar <Ticar@rdacorp.com>
2019-07-30 14:06:04 -0400

    Added heat index/wind chill

--- ScottNet.Web/ClientApp/src/app/weather/tempHumWidget/tempHumWidget.component.js
+++ ScottNet.Web/ClientApp/src/app/weather/tempHumWidget/tempHumWidget.component.js
@@ -16,12 +16,18 @@ var TempHumWidget = /** @class */ (function () {
     ], TempHumWidget.prototype, "humTemp", void 0);
     __decorate([
         core_1.Input()
+    ], TempHumWidget.prototype, "windChill", void 0);
+    __decorate([
+        core_1.Input()
+    ], TempHumWidget.prototype, "heatIndex", void 0);
+    __decorate([
+        core_1.Input()
     ], TempHumWidget.prototype, "icon", void 0);
     TempHumWidget = __decorate([
         core_1.Component({
             selector: 'temp-humidty',
-            template: "\n<div style='width:100%'>\n    <div class='temp-hum'>\n<temperature [temperature]=\"humTemp?.temperature\"></temperature>  / {{humTemp?.humidity}}%\n<img class=\"forecast-icon float-right\" *ngIf=\"icon?.length>0\" [src]=\"icon\" />\n</div>\n\n</div>\n",
-            styles: ["\n    .temp-hum {font-size:22px;}\n    .forecast-icon { width:80px;}\n"]
+            template: "\n<div style='width:100%'>\n    <div class='temp-hum'>\n<temperature [temperature]=\"humTemp?.temperature\"></temperature>  / {{humTemp?.humidity}}%\n<img class=\"forecast-icon float-right\" *ngIf=\"icon?.length>0\" [src]=\"icon\" />\n</div>\n<span *ngIf=\"windChill && windChill < 35\" class=\"small red\" >Wind Chill: {{windChill}} </span>\n<span *ngIf=\"heatIndex && heatIndex > 82\" class=\"small red\">Heat Index: {{heatIndex}} </span>\n</div>\n",
+            styles: ["\n    .red { color: red; }\n    .blue { color: blue; }\n    .temp-hum {font-size:22px;}\n    .forecast-icon { width:80px;}\n"]
         })
     ], TempHumWidget);
     return TempHumWidget;
--- ScottNet.Web/ClientApp/src/app/weather/tempHumWidget/tempHumWidget.component.ts
+++ ScottNet.Web/ClientApp/src/app/weather/tempHumWidget/tempHumWidget.component.ts
@@ -10,10 +10,13 @@ import { CurrentWeatherService, ITemperatureHumidity } from "../../shared";
 <temperature [temperature]="humTemp?.temperature"></temperature>  / {{humTemp?.humidity}}%
 <img class="forecast-icon float-right" *ngIf="icon?.length>0" [src]="icon" />
 </div>
-
+<span *ngIf="windChill && windChill < 35" class="small red" >Wind Chill: {{windChill}} </span>
+<span *ngIf="heatIndex && heatIndex > 82" class="small red">Heat Index: {{heatIndex}} </span>
 </div>
 `,
   styles: [`
+    .red { color: red; }
+    .blue { color: blue; }
     .temp-hum {font-size:22px;}
     .forecast-icon { width:80px;}
 `]
@@ -22,6 +25,8 @@ import { CurrentWeatherService, ITemperatureHumidity } from "../../shared";
 export class TempHumWidget
 {
   @Input() public humTemp: ITemperatureHumidity
+  @Input() public windChill: number
+  @Input() public heatIndex: number
   @Input() public icon: string
 
   constructor(private currentWeather: CurrentWeatherService) {
